798419bb2432c988e04b8e21a19f6f112bc872c3
achmadaffandi785-debug <achmadaffandi785@gmail.com>
2026-09-01 16:34:53 +0700

    Commit pertama: Halo Dunia

---
+++ konsep.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() 
+{
+printf("No : %d\n", 10); 
+printf("Nama  : %s\n", "Ali"); 
+printf("Nilai : %f\n",80.5); 
+printf("Huruf : %c\n",'A'); 
+printf("saya mahasiswa PENS \n");
+printf("nick name : %s\n","bagas rawr");
+}
+
